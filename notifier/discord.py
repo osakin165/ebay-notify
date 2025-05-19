f22d222a9c2502b1ba28ae54638229ff7ce6a434
osakin165 <osakin165@gmail.com>
2025-05-19 11:33:45 +0900

    Update discord.py

--- notifier/discord.py
+++ notifier/discord.py
@@ -1,1 +1,16 @@
+import requests
+from config import DISCORD_WEBHOOK_URL
+
+def send_discord_notification(artist, title, ebay_item):
+    message = f"""📢 **eBay出品通知**
+"**{artist} - {title}**" が見つかりました！
+
+💿 タイトル: {ebay_item['title']}
+💰 価格: {ebay_item['price']} USD
+🔗 [商品ページ]({ebay_item['url']})
+"""
+
+    payload = {"content": message}
+    response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
+    response.raise_for_status()
 
