a0a9b2c415df55b23d15762eba50fd773e1d0db1
osakin165 <osakin165@gmail.com>
2025-05-19 13:16:35 +0900

    Update ebay_client.py

--- ebay_client.py
+++ ebay_client.py
@@ -1,35 +1,36 @@
 import requests
-import os
+from config import EBAY_APP_ID
 
-# 必要に応じて環境変数やconfig.pyに移してください
-EBAY_APP_ID = os.getenv("EBAY_APP_ID")  # 例: "Your-eBay-AppID"
-
-def search_ebay_items(query, category_id="11233", limit=5):
+def search_ebay_items(query):
     url = "https://svcs.ebay.com/services/search/FindingService/v1"
+
     params = {
         "OPERATION-NAME": "findItemsByKeywords",
         "SERVICE-VERSION": "1.0.0",
         "SECURITY-APPNAME": EBAY_APP_ID,
         "RESPONSE-DATA-FORMAT": "JSON",
         "REST-PAYLOAD": "true",
-        "categoryId": category_id,
         "keywords": query,
-        "paginationInput.entriesPerPage": limit,
-        "GLOBAL-ID": "EBAY-US"
+        "paginationInput.entriesPerPage": 5,
+        "GLOBAL-ID": "EBAY-US",
+        "categoryId": "11233"
     }
 
     response = requests.get(url, params=params)
     response.raise_for_status()
+    data = response.json()
 
-    items = []
-    results = response.json().get("findItemsByKeywordsResponse", [])[0].get("searchResult", [])[0].get("item", [])
+    results = []
+    items = data.get("findItemsByKeywordsResponse", [{}])[0].get("searchResult", [{}])[0].get("item", [])
+    for item in items:
+        title = item.get("title", [None])[0]
+        url = item.get("viewItemURL", [None])[0]
+        price = item.get("sellingStatus", [{}])[0].get("currentPrice", [{}])[0].get("__value__")
 
-    for item in results:
-        items.append({
-            "id": item["itemId"][0],
-            "title": item["title"][0],
-            "price": item["sellingStatus"][0]["currentPrice"][0]["__value__"],
-            "url": item["viewItemURL"][0],
+        results.append({
+            "title": title,
+            "url": url,
+            "price": price
         })
 
-    return items
+    return results
