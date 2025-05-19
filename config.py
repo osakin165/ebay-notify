5ed54d17e23755be6ed804d07551bfc213d5d863
osakin165 <osakin165@gmail.com>
2025-05-19 12:28:45 +0900

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 # eBay App ID（承認後にRenderへ追加）
 EBAY_APP_ID = os.getenv("EBAY_APP_ID")
 
-# Excelファイル名（Renderでは事前アップロードが必要）
-EXCEL_FILE_PATH = "ebayプログラム用want list.xlsx"
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+SHEET_NAME = "Wantlist"  # シート名に合わせて変更してOK
